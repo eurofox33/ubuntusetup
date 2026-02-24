aed43331d149ca595cd4bbda61e3b217c3abebf2
eurofox33 <recherche627251@gmail.com>
2026-02-24 05:04:25 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -45,7 +45,7 @@ echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.pro
 echo 'eval "$(pyenv init - bash)"' >> ~/.profile
 
 # Install Python build dependencies
-sudo apt install make build-essential libssl-dev zlib1g-dev \
+sudo apt install -y make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev curl git \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
